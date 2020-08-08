b4c732a88f80458dd01d1a1d89ad998b974d4fc8
fsiddh <jasonshaw546@gmail.com>
2020-08-08 11:22:23 +0530

    updates

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+# SuperUserInformation
+# User: Jose
+# Email: training@pieriandata.com
+# Password: testpassword
 
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
@@ -18,13 +22,14 @@ class Post(models.Model):
         return self.comments.filter(approved_comment=True)
 
     def get_absolute_url(self):
-        return reverse('post_detail', kwargs={'pk':self.pk})
+        return reverse("post_detail",kwargs={'pk':self.pk})
 
     def __str__(self):
         return self.title
 
+
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', related_name='comments')
+    post = models.ForeignKey('blog.Post', related_name='comments',on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
@@ -33,7 +38,7 @@ class Comment(models.Model):
     def approve(self):
         self.approved_comment = True
         self.save()
-    
+
     def get_absolute_url(self):
         return reverse("post_list")
 
